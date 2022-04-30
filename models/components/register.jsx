a30fb90d7ce33e6b010428867442c012e57195e6
Erfan Yousefi <erfanyousefi.co@gmail.com>
2022-04-30 14:59:31 +0430

    Components: create register component

---
+++ models/components/register.jsx
@@ -0,0 +1,10 @@
+function Register(){
+    return (<>
+        <h1>Register Form</h1>
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+    </>)
+}
