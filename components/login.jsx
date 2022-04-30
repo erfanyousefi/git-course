67c39491c2a5ae0d73e8d22a5b3dc53c9e3bd8c2
Erfan Yousefi <erfanyousefi.co@gmail.com>
2022-04-30 15:28:07 +0430

    Merge Components with master

---
+++ components/login.jsx
@@ -0,0 +1,7 @@
+function Login(){
+    return <>
+    <div>Login form</div>
+    <input type="text" name="username" id="username" />
+    <input type="password" name="password" id="password" />
+    </>
+}
---
+++ components/register.jsx
@@ -0,0 +1,10 @@
+function Register(){
+    return (<>
+        <h1>Register Form</h1>
+        <input type="text" name="name" id="name" />
+        <input type="text" name="username" id="username" />
+        <input type="text" name="email" id="email" />
+        <input type="text" name="phone" id="phone" />
+        <input type="text" name="password" id="password" />
+    </>)
+}
