aa024562eac0415b339ef573791245f1fad5ac0c
Erfan Yousefi <erfanyousefi.co@gmail.com>
2022-05-01 23:44:56 +0430

    Controller: create folder

--- models/blogs.js
+++ models/blogs.js
@@ -1,1 +1,11 @@
 //blogs model
+// fetch vs pull
+// pull : branch => pull request (......)
+//fetch : total => git(repositry)
+
+// merge vs rebase    |----O----O|
+// O---O---O--|O----O---O---O----O-O---O
+//     |__O___O    |___O___O|
+
+//O----O-----O-O---O---O---O---O---O
+//                     
